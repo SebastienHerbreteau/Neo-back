34bb7f239f5f7b793a50b7273cf35b2408491e9b
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2023-01-06 14:39:34 +0100

    routeur

--- index.php
+++ index.php
@@ -18,47 +18,45 @@ Database::$pass = $_ENV['DBPASSWORD'];
 Database::$dbName = $_ENV['DBNAME'];
 Database::connect();
 
-try {
-    if ($_SERVER['REQUEST_URI'] === '/agency') {
-        if (isset($_GET['agency_id'])) {
-            $id = $_GET['agency_id'];
-            $AgencyController = new AgencyController();
-            $data = $AgencyController->getAgency($id);
-            echo $AgencyController->ToJSON($data);
-        }
-        if (isset($_GET['agency_by_planet'])) {
-            echo 'success';
-            $id = $_GET['agency_by_planet'];
-            $AgencyController = new AgencyController();
-            $data = $AgencyController->getAgencyByPlanetId($id);
-            echo $AgencyController->ToJSON($data);
-        } else {
-            $AgencyController = new AgencyController();
-            $data = $AgencyController->getAllAgency();
-            echo $AgencyController->ToJSON($data);
-        }
+if ($_SERVER['REQUEST_URI'] === '/agency') {
+    if (isset($_GET['agency_id'])) {
+        $id = $_GET['agency_id'];
+        $AgencyController = new AgencyController();
+        $data = $AgencyController->getAgency($id);
+        echo $AgencyController->ToJSON($data);
     }
+    if (isset($_GET['agency_by_planet'])) {
+        echo 'success';
+        $id = $_GET['agency_by_planet'];
+        $AgencyController = new AgencyController();
+        $data = $AgencyController->getAgencyByPlanetId($id);
+        echo $AgencyController->ToJSON($data);
+    } else {
+        $AgencyController = new AgencyController();
+        $data = $AgencyController->getAllAgency();
+        echo $AgencyController->ToJSON($data);
+    }
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $pwd = $_POST['pwd'];
+        $ceo_name = $_POST['ceo_name'];
+        $tel = $_POST['tel'];
+        $website = $_POST['website'];
+        $logo = $_POST['logo'];
+        $id_planet = $_POST['id_planet'];
 
-    //     $name = $_GET['name'];
-    //     $email = $_GET['email'];
-    //     $pwd = $_GET['pwd'];
-    //     $ceo_name = $_GET['ceo_name'];
-    //     $tel = $_GET['tel'];
-    //     $website = $_GET['website'];
-    //     $logo = $_GET['logo'];
-    //     $id_planet = $_GET['id_planet'];
-
-    // ?name=aze&email=aze&pwd=aze&ceo_name=aze&tel=123&website=AZE&$logo=url&id_planet=1
-
-    //     $name,
-    //     $email,
-    //     $pwd,
-    //     $ceo_name,
-    //     $tel,
-    //     $website,
-    //     $logo,
-    //     $id_planet
-} catch (Exception $e) {
-    $errorMessage = $e->getMessage();
-    /* envoyer JSON erreur*/
+        $AgencyController = new AgencyController();
+        $data = $AgencyController->postAgency(
+            $name,
+            $email,
+            $pwd,
+            $ceo_name,
+            $tel,
+            $website,
+            $logo,
+            $id_planet
+        );
+        echo $AgencyController->ToJSON($data);
+    }
 }
