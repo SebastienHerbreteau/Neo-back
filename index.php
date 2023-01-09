7ccd8fbd52799fb7527568089826894d6fca701e
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2023-01-09 10:28:49 +0100

    routeur-candidate

--- index.php
+++ index.php
@@ -18,22 +18,16 @@ Database::$pass = $_ENV['DBPASSWORD'];
 Database::$dbName = $_ENV['DBNAME'];
 Database::connect();
 
-// ROUTER #######################################################################"
-
-// AGENCY ==========================================================
-
+//--------------------------------------------------------- ROUTEUR AGENCY------------------------------------------------
 // return all of the agencies
-if ($_SERVER['REQUEST_URI'] == '/agency')
-{
+if ($_SERVER['REQUEST_URI'] == '/agency') {
     $AgencyController = new AgencyController();
     $data = $AgencyController->getAllAgency();
     echo $AgencyController->ToJSON($data);
 }
 
-
 // return an agency by it's id
-if (isset($_GET['id']) && str_contains($_SERVER['REQUEST_URI'], "/agency"))
-{
+if (isset($_GET['id']) && str_contains($_SERVER['REQUEST_URI'], '/agency')) {
     $id = $_GET['id'];
     $AgencyController = new AgencyController();
     $data = $AgencyController->getAgency($id);
@@ -41,8 +35,7 @@ if (isset($_GET['id']) && str_contains($_SERVER['REQUEST_URI'], "/agency"))
 }
 
 // Return agencies by their home planet ID
-if (isset($_GET['planet']))
-{
+if (isset($_GET['planet'])) {
     $id = $_GET['planet'];
     $AgencyController = new AgencyController();
     $data = $AgencyController->getAgencyByPlanetId($id);
@@ -50,20 +43,18 @@ if (isset($_GET['planet']))
     return;
 }
 
-//Add an agency
 if (
-$_SERVER['REQUEST_METHOD'] == 'POST' &&
-str_contains($_SERVER['REQUEST_URI'], "/add-agency") &&
-!empty($_POST['name']) and
-!empty($_POST['email']) and
-!empty($_POST['pwd']) and
-!empty($_POST['ceo_name']) and
-!empty($_POST['tel']) and
-!empty($_POST['website']) and
-!empty($_POST['logo']) and
-!empty($_POST['id_planet'])
-) 
-{
+    $_SERVER['REQUEST_METHOD'] == 'POST' &&
+        str_contains($_SERVER['REQUEST_URI'], '/add-agency') &&
+        !empty($_POST['name']) and
+    !empty($_POST['email']) and
+    !empty($_POST['pwd']) and
+    !empty($_POST['ceo_name']) and
+    !empty($_POST['tel']) and
+    !empty($_POST['website']) and
+    !empty($_POST['logo']) and
+    !empty($_POST['id_planet'])
+) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
@@ -85,21 +76,20 @@ str_contains($_SERVER['REQUEST_URI'], "/add-agency") &&
         $id_planet
     );
 }
-//Modify an agency by ID
+
 if (
-$_SERVER['REQUEST_METHOD'] == 'POST' &&
-str_contains($_SERVER['REQUEST_URI'], "/modify-agency") &&
-!empty($_POST['id']) and
-!empty($_POST['name']) and
-!empty($_POST['email']) and
-!empty($_POST['pwd']) and
-!empty($_POST['ceo_name']) and
-!empty($_POST['tel']) and
-!empty($_POST['website']) and
-!empty($_POST['logo']) and
-!empty($_POST['id_planet'])
-) 
-{
+    $_SERVER['REQUEST_METHOD'] == 'POST' &&
+        str_contains($_SERVER['REQUEST_URI'], '/modify-agency') &&
+        !empty($_POST['id']) and
+    !empty($_POST['name']) and
+    !empty($_POST['email']) and
+    !empty($_POST['pwd']) and
+    !empty($_POST['ceo_name']) and
+    !empty($_POST['tel']) and
+    !empty($_POST['website']) and
+    !empty($_POST['logo']) and
+    !empty($_POST['id_planet'])
+) {
     $id = $_POST['id'];
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -123,19 +113,113 @@ str_contains($_SERVER['REQUEST_URI'], "/modify-agency") &&
         $id_planet
     );
 }
-
-
-// Delete an agency by ID
-
-
-if (isset($_GET['id']) && str_contains($_SERVER['REQUEST_URI'], "/delete-agency"))
-{
+if (
+    isset($_GET['id']) &&
+    str_contains($_SERVER['REQUEST_URI'], '/delete-agency')
+) {
+    var_dump($id);
     $id = $_GET['id'];
     $AgencyController = new AgencyController();
     $data = $AgencyController->deleteAgency($id);
     echo $AgencyController->ToJSON($data);
 }
 
+//--------------------------------------------------------- ROUTEUR CANDIDATE------------------------------------------------
 
+if ($_SERVER['REQUEST_URI'] == '/candidate') {
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->getAllCandidates();
+    echo $CandidateController->ToJSON($data);
+}
 
+// return an Candidate by it's id
+if (isset($_GET['id']) && str_contains($_SERVER['REQUEST_URI'], '/candidate')) {
+    $id = $_GET['id'];
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->getCandidate($id);
+    echo $CandidateController->ToJSON($data);
+}
 
+// Return agencies by their home planet ID
+if (isset($_GET['planet'])) {
+    $id = $_GET['planet'];
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->getCandidateByJobOffer($id);
+    echo $CandidateController->ToJSON($data);
+    return;
+}
+
+if (
+    $_SERVER['REQUEST_METHOD'] == 'POST' &&
+        str_contains($_SERVER['REQUEST_URI'], '/add-candidate') &&
+        !empty($_POST['name']) and
+    !empty($_POST['email']) and
+    !empty($_POST['pwd']) and
+    !empty($_POST['tel']) and
+    !empty($_POST['avatar']) and
+    !empty($_POST['cv']) and
+    !empty($_POST['id_planet'])
+) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+    $tel = $_POST['tel'];
+    $avatar = $_POST['avatar'];
+    $cv = $_POST['cv'];
+    $id_planet = $_POST['id_planet'];
+
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->postCandidate(
+        $name,
+        $email,
+        $pwd,
+        $tel,
+        $avatar,
+        $cv,
+        $id_planet
+    );
+}
+
+if (
+    $_SERVER['REQUEST_METHOD'] == 'POST' &&
+        str_contains($_SERVER['REQUEST_URI'], '/modify-candidate') &&
+        !empty($_POST['id']) and
+    !empty($_POST['name']) and
+    !empty($_POST['email']) and
+    !empty($_POST['pwd']) and
+    !empty($_POST['tel']) and
+    !empty($_POST['avatar']) and
+    !empty($_POST['cv']) and
+    !empty($_POST['id_planet'])
+) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+    $tel = $_POST['tel'];
+    $avatar = $_POST['avatar'];
+    $cv = $_POST['cv'];
+    $id_planet = $_POST['id_planet'];
+
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->putCandidate(
+        $id,
+        $name,
+        $email,
+        $pwd,
+        $tel,
+        $avatar,
+        $cv,
+        $id_planet
+    );
+}
+if (
+    isset($_GET['id']) &&
+    str_contains($_SERVER['REQUEST_URI'], '/delete-candidate')
+) {
+    var_dump($id);
+    $id = $_GET['id'];
+    $CandidateController = new CandidateController();
+    $data = $CandidateController->deleteCandidate($id);
+    echo $CandidateController->ToJSON($data);
+}
